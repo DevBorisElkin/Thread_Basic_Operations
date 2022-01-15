a55dca85230333195afbd25d4db69d9345d1aa16
DevBorisElkin <ea.foreverrr@gmail.com>
2022-01-15 12:28:54 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thread_Basic_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
